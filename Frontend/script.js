c2fcda60212a262a0f3d1ebacf8b05dc155754f6
Vishal Goswami <vishalgoswami.01010@gmail.com>
2026-09-07 20:52:25 +0530

    changing API
    Deploy api and add in js

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://task-management-app-2-hfeq.onrender.com";
 
 /* ==================== Toast ==================== */
 let toastTimer = null;
